c58d675435903b6ea6bcee7b04cc13addba9fe06
Ziqi Wang <ziqi.wang39@gmail.com>
2021-08-19 11:19:15 +1000

    Update server code

--- index.js
+++ index.js
@@ -7,58 +7,75 @@ app.use(express.json());
 
 
 app.post('/tasks',(req,res)=>{
-    //console.log(req.body)
-    data.push(req.body);
-    res.json(req.body);
+    console.log(req.body)
+    if(req.body.id === data.length+1 && req.body.description !==''&& req.body.done === false){
+        data.push(req.body);
+        res.status(201).json('The task successfully added')
+        //res.json(req.body);
+    }else{
+        res.json("The task you post is incorrect!!!");
+    }
+    
 })
 
 app.get('/tasks',(req,res)=>{
     //console.log(data)
-    res.json(data)
+    res.status(200).json(data)
 })
 
 app.get('/tasks/:id',(req,res)=>{
    const {id}=req.params;
-   let task;
-   data.forEach((item)=>{
-       if(item.id === parseInt(id)){
-        task = item;
+   let index=-1;
+   for (let task of data){
+      if(task.id === parseInt(id)){
+          index = id-1;
        }
-   })
-   //console.log(task)
-   res.json(task)
+   }
+   if(index>=0){
+       let task = data[index];
+       res.status(200).json(task)
+   }else{
+       res.status(404).send('Task not found.')
+   }
 })
-
 app.put('/tasks/:id',(req,res)=>{
     const {id}=req.params;
     // let description = req.body.description;
     let done = req.body.done;
-    let task;
 
-    data.forEach((item)=>{
-        if(item.id === parseInt(id)){
-         task = item;
-        }
-    })
-    // task.description = description;
-    task.done = done;
-    res.json(task);
-  
+    let index=-1;
+    for (let task of data){
+      if(task.id === parseInt(id)){
+          index = id-1;
+       }
+    }
+    if(index>=0){
+       let task = data[index];
+       task.done = done;
+       res.status(200).json(task);
+       
+    }else{
+       res.status(404).send('Task not found!!!')
+    }
+   
 })
 
 app.delete('/tasks/:id',(req,res)=>{
     const {id}=req.params;
-    let task;
-    let taskIndex;
-
-    data.forEach((item, index)=>{
-        if(item.id === parseInt(id)){
-         task = item;
-         taskIndex = index;
-        }
-    })
-    data.splice(task, 1)
-    res.json()
+    let index=-1;
+    for (let task of data){
+      if(task.id === parseInt(id)){
+          index = id-1;
+       }
+    }
+    if(index>=0){
+       data.splice(index, 1)
+       res.status(204).send('The task successfully deleted')
+       
+    }else{
+       res.status(404).send('Task not found!!!')
+    }
+    
 })
 
 
